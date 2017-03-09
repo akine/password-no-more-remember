90a9531dd54913c6a53832d28d372c614f69dee5
akinechan <weezer.and.you@gmail.com>
2017-03-09 01:49:18 +0000

    language JP to EN

--- password.rb
+++ password.rb
@@ -2,6 +2,6 @@ def pw()
   [*'A'..'Z', *'a'..'z', *0..9].tap { |a| break 8.times.map { a.sample }.join }
 end
   
-puts "何のパスワードが欲しいんや？"
+puts "So, what password do you want to change?"
 service = gets.chomp
-puts "よし！今日から#{service}のパスワードはこれや！\n|｡･ω･)ﾉ⌒#{pw}"
+puts "For now your \"#{service}\" password is here!\n(*｡･ω･)っ#{pw}"
